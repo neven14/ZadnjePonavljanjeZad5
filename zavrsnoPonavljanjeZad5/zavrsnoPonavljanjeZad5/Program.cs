78793d76a4df475c51c5f409b613b0b39abf867b
Neven <58472164+neven14@users.noreply.github.com>
2021-06-12 12:31:03 +0200

    Add files via upload

---
+++ zavrsnoPonavljanjeZad5/zavrsnoPonavljanjeZad5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsnoPonavljanjeZad5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1, i2, i3, i4, i5, i6, i7;
+
+            for (i1 = 1; i1 <= 33; i1++)
+            {
+                for (i2 = i1 + 1; i2 <= 34; i2++)
+                {
+                    for (i3 = i2 + 1; i3 <= 35; i3++)
+                    {
+                        for (i4 = i3 + 1; i4 <= 36; i4++)
+                        {
+                            for (i5 = i4 + 1; i5 <= 37; i5++)
+                            {
+                                for (i6 = i5 + 1; i6 <= 38; i6++)
+                                {
+                                    for (i7 = i6 + 1; i7 <= 39; i7++)
+                                    {
+                                    Console.WriteLine(i1 + " " + i2 + " " + i3 + " " + i4 + " " + i5 + " " + i6 + " " + i7);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
